944a364a1439b277ef1b5008bfbe3531af599f7e
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-08-26 23:34:10 -0300

    api7

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -66,7 +66,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 50,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 TEMPLATES = [
